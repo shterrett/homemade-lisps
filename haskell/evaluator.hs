1c94bc7340bb39afc9bc395cf3f2a0441debb470
Stuart Terrett <shterrett@gmail.com>
2014-10-22 07:42:10 -0400

    Evals basic numeric ops on list of integers

--- haskell/evaluator.hs
+++ haskell/evaluator.hs
@@ -1,5 +1,6 @@
 module Evaluator where
 import LispValue
+import Numeric
 
 eval :: LispVal -> LispVal
 eval val@(String _) = val
@@ -10,3 +11,28 @@ eval val@(Float _) = val
 eval val@(Ratio _) = val
 eval val@(Complex _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                        if null parsed
+                          then 0
+                          else fst $ head parsed
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
