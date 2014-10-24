f94ec8676c99d3967b0d65e4175c3ae8e8f3449b
Stuart Terrett <shterrett@gmail.com>
2014-10-24 07:16:25 -0400

    Adds `if` statement

--- haskell/evaluator.hs
+++ haskell/evaluator.hs
@@ -14,6 +14,11 @@ eval val@(Float _) = return val
 eval val@(Ratio _) = return val
 eval val@(Complex _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) = do
+   result <- eval pred
+   case result of
+     Bool False -> eval alt
+     otherwise -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized Special Form" [badForm]
 
